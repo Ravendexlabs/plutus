f66f829e4f07e6f1d3fe82a739ac88740ce8ffa5
Shea Levy <shea@shealevy.com>
2021-10-18 09:35:26 -0400

    FIXUP after review: Deployed web-ghc needs marlowe

--- bitte/default.nix
+++ bitte/default.nix
@@ -4,10 +4,6 @@ let
   playgroundStatic = pkgs.callPackage ./playground-static.nix { inherit staticSite; docs = docs.site; };
 in
 {
-  web-ghc-server-entrypoint = pkgs.callPackage ./web-ghc-server.nix {
-    web-ghc-server = web-ghc;
-  };
-
   plutus-playground-server-entrypoint = pkgs.callPackage ./plutus-playground-server.nix {
     variant = "plutus";
     pkg = plutus-playground.server;
